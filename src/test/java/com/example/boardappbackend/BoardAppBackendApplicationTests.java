0a44229dc6a05925b3f9ca412823a20d554a6f5b
bit\bitcamp <1>
2024-09-27 10:44:05 +0900

    board-app-back

---
+++ src/test/java/com/example/boardappbackend/BoardAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
